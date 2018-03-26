b24468a1d5f3eceb3b5938c30eb30ee144f2821a
danist7 <daniel.santo-tomas@estudiante.uam.es>
2018-03-26 17:38:08 +0200

    profile co un cambio lol

--- 1000house/src/App/Profile.java
+++ 1000house/src/App/Profile.java
@@ -10,21 +10,21 @@ package App;
 public abstract class Profile {
 	
 	private String ccNumber;
-	private Aplication app;
+	
 	
 	
 	
 
-	public Profile(String ccNumber, Aplication app) {
+	public Profile(String ccNumber) {
 		this.ccNumber = ccNumber;
-		this.app = app;
+		
 	}
 
 	public String getccNumber() {
 		return ccNumber;
 	}
 
-	public Boolean changeCCNumber(String cCNumber, String username, String password) {
+	public Boolean changeCCNumber(String cCNumber, String username, String password, Aplication app) {
 		if(app.getAdmUsername().equals(username) == true && app.getAdmPassword().equals(password) == true) {
 			ccNumber = cCNumber;
 			return true;
