9bdf34a21784b629ba8cfeb5965e6189d7effb73
danist7 <32202733+danist7@users.noreply.github.com>
2018-04-03 19:48:41 +0200

    VIVA

---
+++ ApplicationTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package testers;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+
+import org.junit.*;
+import app.*;
+import exception.*;
+
+/**
+ * @author Daniel Santo-Tomas daniel.santo-tomas@estudiante.uam.es
+ * @author Lucia Rivas Molina lucia.rivas@estudiante.uam.es
+ *
+ */
+public class ApplicationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUpBefore() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
