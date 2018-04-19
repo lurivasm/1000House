1960b42b8c51f3860815414b5eaa236fa1d2e378
Lucía Rivas <lucia_rm98@hotmail.com>
2018-04-19 19:47:23 +0200

    house

---
+++ Prueba/src/coso/House.java
@@ -0,0 +1,20 @@
+package coso;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class House extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel textPanel = new JPanel();
+	private JLabel title;
+	
+	public House() {
+		super("Create a House!");
+	}
+
+}
