fe7f14c6c3cbc8ab33dffdc56ccfabf38315b4e9
Lucía Rivas <lucia_rm98@hotmail.com>
2018-05-02 22:38:19 +0200

    create offer

--- MilHouse/src/windows/prueba.java
+++ MilHouse/src/windows/prueba.java
@@ -1,17 +1,28 @@
 package windows;
 
-import app.Application;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.*;
 import controllers.*;
 
 public class prueba {
 
 	public static void main(String[] args) throws Exception {
-		LoginWindow j = new LoginWindow();
-		Application app = new Application("fDGAdsaf");
-		LoginController c1 = new LoginController(j,app);
-		LogoutController c2 = new LogoutController(j,app);
-		j.setLoginController(c1);
-		j.setLogoutController(c2);
+		House h1;
+		House h2;
+		User host;
+		Application app;
+		List<Characteristics> c = new ArrayList<Characteristics>() ;
+		app = new Application("Test");
+		host = new User( "Lucia", "Rivas Molina", "Gnomo69", "12796482F", "OD", "12345678" ,app);
+		h1 = new House("LOCATION1", "DESCRIPTION1", c, 28964,host);
+		h2 = new House("LOCATION2", "DESCRIPTION2", c, 28974,host);
+		ArrayList <House> houses = new ArrayList <House>();
+		houses.add(h1);
+		houses.add(h2);
+		
+		CreateOfferWindow j = new CreateOfferWindow(houses);
 	}
 
 }
--- MilHouse/src/windows/prueba2.java
+++ MilHouse/src/windows/prueba2.java
@@ -6,28 +6,12 @@ import java.util.List;
 
 import app.*;
 import exception.*;
+import app.*;
 
 public class prueba2 {
 
 	public static void main(String[] args) throws Exception{
-		List<Offer> l = new ArrayList<Offer>();
-		Application app = new Application("gd");
-		User u1 = new User( "Lucia", "Rivas Molina", "Gnomo69", "12796482F", "OD", "12345678" ,app);
-		u1.setState(UserStates.CONNECTED_HOST);
-		House ha = new House("micasa" ,"jhf", new ArrayList<Characteristics>(),9875 ,u1);
-		LocalDate init = LocalDate.of(2018, 4, 28);
-		Offer offer18 =  new LivingOffer(init, 1000, 200,ha, app, 4);
-		l.add(offer18);
-		for(int i = 0;i < 10;i++) {
-			House h = new House("Potato street Madrid" + i,"jhf", new ArrayList<Characteristics>(),47625 ,u1);
-			LocalDate ini = LocalDate.of(2018, 4, 28);
-			Offer offer1 =  new LivingOffer(ini, 1000, 200,h, app, 4);
-			LocalDate end =  LocalDate.of(2018, 5, 10);
-			Offer offer2 =  new HolidaysOffer(ini, 100, 200,h, app, end);
-			l.add(offer1);
-			l.add(offer2);			
-		}
-		searchWindow w = new searchWindow(l);
+		HouseWindow h = new HouseWindow();
 		
 	}
 
