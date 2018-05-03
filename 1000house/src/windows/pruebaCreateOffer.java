61982dc017740df1c2b7e2251e0ab84d6f7b15a7
Lucía Rivas <lucia_rm98@hotmail.com>
2018-05-03 19:47:21 +0200

    windows

---
+++ 1000house/src/windows/pruebaCreateOffer.java
@@ -0,0 +1,33 @@
+package windows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.*;
+import controllers.*;
+
+public class pruebaCreateOffer {
+
+	public static void main(String[] args) throws Exception {
+		House h1;
+		House h2;
+		User host;
+		Application app;
+		List<Characteristics> c = new ArrayList<Characteristics>() ;
+		app = new Application("Test");
+		host = new User( "Lucia", "Rivas Molina", "Gnomo69", "12796482F", "OD", "12345678" ,app);
+		host.setState(UserStates.CONNECTED_HOST);
+		app.setLog(host);
+		
+		app.createHouse("LOCCCCCCCCCC", c, "ZIII", 28964);
+		app.createHouse("LOCCCCCAFAASD", c, "WWWI", 28974);		
+		
+		CreateOfferWindow j = new CreateOfferWindow(app.getLog().getHostProfile().getHouses());
+		CreateOfferController cont = new CreateOfferController(j, app);
+		j.setCreateOfferController(cont);
+		MenuController menu = new MenuController(j, app);
+		j.setMenuController(menu);
+	
+	}
+
+}
---
+++ 1000house/src/windows/pruebaHouse.java
@@ -0,0 +1,25 @@
+package windows;
+
+
+
+import app.*;
+import controllers.*
+;public class pruebaHouse {
+
+	public static void main(String[] args) throws Exception{
+		User host;
+		Application app;
+		app = new Application("Test");
+		host = new User( "Lucia", "Rivas Molina", "Gnomo69", "12796482F", "OD", "12345678" ,app);
+		host.setState(UserStates.CONNECTED_HOST);
+		app.setLog(host);
+		HouseWindow h = new HouseWindow();
+		
+		HouseController cont = new HouseController(h, app);
+		h.setHouseController(cont);
+		
+		MenuController menu = new MenuController(h, app);
+		h.setMenuController(menu);
+	}
+
+}
